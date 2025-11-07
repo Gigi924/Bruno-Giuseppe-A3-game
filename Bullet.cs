465b072da6c299ebd875a7f06f9ae17a02175b01
giuseppe <000981592@mohawkcollege.ca>
2025-11-07 12:16:59 -0500

    set up Bullet class

---
+++ Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    internal class Bullet
+    {
+        Vector2 pos;
+        Vector2 size;
+
+        public Bullet(Vector2 pos, Vector2 size)
+        {
+            this.pos = pos;
+            this.size = size;
+        }
+    }
+}
