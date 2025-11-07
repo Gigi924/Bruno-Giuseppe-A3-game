b18947f0a53b14809bc6ba6f15140c746cbe7292
giuseppe <000981592@mohawkcollege.ca>
2025-11-07 12:05:29 -0500

    setup player class

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using MohawkGame2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohawkGame2D
+{
+    internal class player
+    {
+        Vector2 pos;
+        Vector2 size;
+        public player(Vector2 pos, Vector2 size)
+        {
+            this.pos = pos;
+            this.size = size;
+        }
+
+
+    }
+}
